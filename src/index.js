b162be34ca018608304e5240d12fb385c2f47a02
Anna Kotova <kotovanna2001@gmail.com>
2025-01-23 21:49:17 +0300

    fix problems

--- src/index.js
+++ src/index.js
@@ -13,44 +13,33 @@ const genDiff = (filePath1, filePath2, formatName = 'stylish') => {
 
   const fileType = path.extname(filePath1).slice(1);
 
-  let file1ParsedData = '';
-  let file2ParsedData = '';
-
-  if (fileType === 'json') {
-    file1ParsedData = jsonParse(file1Data);
-    file2ParsedData = jsonParse(file2Data);
-  } else {
-    file1ParsedData = yamlParse(file1Data);
-    file2ParsedData = yamlParse(file2Data);
-  }
+  const file1ParsedData = fileType === 'json' ? jsonParse(file1Data) : yamlParse(file1Data);
+  const file2ParsedData = fileType === 'json' ? jsonParse(file2Data) : yamlParse(file2Data);
 
   const findDifferences = (obj1, obj2) => {
     const keys = _.union(Object.keys(obj1), Object.keys(obj2));
     const sortedKeys = _.sortBy(keys);
-    const result = [];
-    sortedKeys.forEach((key) => {
+    const result = sortedKeys.map((key) => {
       if (Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
         if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-          result.push({ key, state: 'nested', children: findDifferences(obj1[key], obj2[key]) });
-          return;
+          return { key, state: 'nested', children: findDifferences(obj1[key], obj2[key]) };
         }
-        if (obj1[key] === obj2[key]) {
-          result.push({ key, state: 'unchanged', value: obj1[key] });
-        }
+
         if (obj1[key] !== obj2[key]) {
-          result.push({
+          return {
             key, state: 'changed', oldValue: obj1[key], newValue: obj2[key],
-          });
+          };
         }
       }
 
       if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-        result.push({ key, state: 'removed', value: obj1[key] });
+        return { key, state: 'removed', value: obj1[key] };
       }
 
       if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-        result.push({ key, state: 'added', value: obj2[key] });
+        return { key, state: 'added', value: obj2[key] };
       }
+      return { key, state: 'unchanged', value: obj1[key] };
     });
     return result;
   };
